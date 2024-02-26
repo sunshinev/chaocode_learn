4a98fa6d14beb830e744031fbd3bbdf061b092ea
jaysun <947233713@qq.com>
2024-02-26 23:01:25 +0800

    learn

---
+++ FoodPicer/FoodPicer/Screen/HomeScreen.swift
@@ -0,0 +1,18 @@
+//
+//  HomeScreen.swift
+//  FoodPicer
+//
+//  Created by jaysun on 2024/2/26.
+//
+
+import SwiftUI
+
+struct HomeScreen: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    HomeScreen()
+}
